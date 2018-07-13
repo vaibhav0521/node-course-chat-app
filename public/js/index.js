6cfba344c8b96923fb93b920e41b8537eb70247f
vaibhav <vaibhavagarwal606@gmail.com>
2018-07-13 15:27:25 +0530

    emit new message

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,7 @@ var socket=io();
 
 socket.on('connect',function(){
   console.log("connected to server");
-
-
-
-  socket.emit('createMessage',{
-    from:"vaibhav",
-    text:"hey!"
-  })
-});
+})
 
 
 
@@ -17,6 +10,6 @@ socket.on('disconnect',function (){
   console.log("server disconnected");
 })
 
-socket.on('newMessage',function(message){
-  console.log("email:",message);
+socket.on('newMessage', function(message){
+  console.log("message:",message);
 })
--- server/server.js
+++ server/server.js
@@ -15,16 +15,15 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
   console.log("new user connected");
 
-  socket.emit('newMessage',{
-      from:"vaibhav",
-      text:"hello!",
-      createdAt:"1234"
-  });
-
 
   socket.on('createMessage',(message)=>{
     console.log("message:",message);
-  })
+    io.emit('newMessage',{
+      from:message.from,
+      text:message.text,
+      createdAt: new Date().getTime()
+    });
+  });
 
   socket.on('disconnect',()=>{
     console.log("user was disconnected")
